4f94cef01fdfe16f4ed483b7952ace90543a6d53
Oliver <oliver.friedrich@gmail.com>
2016-01-23 12:56:23 +0100

    add first 2 solutions to ProjectEuler problems

---
+++ ProjectEuler/1.fs
@@ -0,0 +1,7 @@
+﻿// https://projecteuler.net/problem=1
+module Problem1
+
+let solution =
+    [1 .. 999]
+    |> List.filter (fun x -> x % 3 = 0 || x % 5 = 0)
+    |> List.sum
---
+++ ProjectEuler/2.fs
@@ -0,0 +1,17 @@
+﻿// https://projecteuler.net/problem=2
+module Problem2
+
+let prependNext (arr : int[]) =
+    Array.append [| arr.[0] + arr.[1] |] arr
+
+let prependUpTo limit (arr : int[]) =
+    let mutable newArr = arr
+    while newArr.[0] < limit
+        do newArr <- prependNext newArr
+    newArr
+
+let solution =
+    [| 2; 1 |]
+    |> prependUpTo 4000000
+    |> Array.filter (fun i -> i <= 4000000 && i % 2 = 0)
+    |> Array.sum
